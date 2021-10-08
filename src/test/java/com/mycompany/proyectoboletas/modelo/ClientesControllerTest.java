a7293fcd72a14e9f7cbed7872927ac4f3a96d4a3
Jorge Mainhard Bravo <63559099+jmainhard@users.noreply.github.com>
2021-10-07 21:04:27 -0300

    Updating dependencies

---
+++ src/test/java/com/mycompany/proyectoboletas/modelo/ClientesControllerTest.java
@@ -0,0 +1,16 @@
+/*
+ *  @author Esteban E., Jorge M.
+ *  07-10-2021
+ */
+
+package com.mycompany.proyectoboletas.modelo;
+
+import org.junit.jupiter.api.Test;
+
+public class ClientesControllerTest {
+
+    @Test
+    public void testExisteCliente() {
+
+    }
+}
