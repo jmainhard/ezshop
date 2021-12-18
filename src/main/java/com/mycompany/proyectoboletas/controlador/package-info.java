2fe2f8a46ca0eb11433d27531b438984af968aee
Esteban Esparza <e.esparza02@ufromail.cl>
2021-12-17 21:19:29 -0300

    Documentación packages

---
+++ src/main/java/com/mycompany/proyectoboletas/controlador/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * com.mycompany.proyectoboletas.controlador es el paquete encargado de almacenar las clases encargadas de manejar
+ * y trabajar los datos de la aplicación.
+ */
+
+package com.mycompany.proyectoboletas.controlador;
